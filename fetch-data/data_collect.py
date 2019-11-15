aa425920c0c1c0d64cc7d87c7fabb3258c942207
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2019-11-15 18:10:55 -0500

    [TASK] Added python script to fetch data from infura node

---
+++ fetch-data/data_collect.py
@@ -0,0 +1,44 @@
+import sys
+import infura
+from datetime import datetime
+
+class DataCollect:
+    def __init__(self):
+        self.infura = infura.INFURA(
+            _project_id="",
+            _project_scrt=""
+        )
+
+    def get_block_by_number(self):
+        # https://etherscan.io/block/8900401
+        block_hash = "0xab55941eddeb3103febf60ec325dfd8d8ca57fac0104d4ae1d63749fb68db465"
+        count = 0
+        max_count = 100
+        while (count != max_count and block_hash != None):
+            response = self.infura.get_block_by_hash(block_hash)
+            result = response["result"]
+            timestamp = int(result["timestamp"], 16)
+            file_name = "data/" + datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d_%H-%M-%S') + "_" + str(int(result["number"], 16)) + ".txt"
+            op_file = open(file_name, 'w')
+            block_hash = result["parentHash"] 
+            transactions = result["transactions"]
+            for i in range(0, len(transactions)):
+                self.parse_transaction(op_file, transactions[i])
+            op_file.close()            
+            count += 1
+
+    def parse_transaction(self, op_file, txn):
+        to_addr = txn["to"]
+        from_addr = txn["from"]
+        value = str(int(txn["value"], 16))
+        hash = txn["hash"]
+        txn_idx = str(int(txn["blockNumber"], 16))+str(int(txn["transactionIndex"], 16))
+        if (to_addr and from_addr and value and txn_idx and hash):
+            op = hash + "," + to_addr + "," + from_addr + "," + value + "," + txn_idx + "\n"
+            op_file.write(op)
+
+
+
+if __name__ == "__main__":
+    data_collector = DataCollect()
+    block = data_collector.get_block_by_number()
